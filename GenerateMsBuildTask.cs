85b5f8eb727f05e9c7dd8763bfabfdc1c68b3129
Leszek 'skolima' Ciesielski <skolima@gmail.com>
2011-02-07 21:50:01 +0100

    Base task skeleton.

---
+++ GenerateMsBuildTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace GenerateMsBuildTask
+{
+    [TaskName("generate-msbuild")]
+    public class GenerateMsBuildTask : Task
+    {
+        protected override void ExecuteTask()
+        {
+            Project.BuildFinished += BuildFinished;
+            Project.TaskFinished += TaskFinished;
+        }
+
+        protected void BuildFinished(object sender, BuildEventArgs args)
+        {
+
+        }
+
+        protected void TaskFinished(object sender, BuildEventArgs args)
+        {
+            if (args.Exception == null)
+            {
+                switch(args.Task.Name)
+                {
+                    case "csc": break;
+                    case "resgen": break;
+                    case "mkdir": break;
+                    case "delete": break;
+                    case "nant": break; // TODO: this has to be handled in TaskStarted to properly handle chaining
+                }
+            }
+        }
+    }
+}
