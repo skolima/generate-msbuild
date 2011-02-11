afc28cb09ccdc134b17f3a76f352b4e61206fe93
Leszek 'skolima' Ciesielski <skolima@gmail.com>
2011-02-11 22:28:20 +0100

    Proper registration, so that the listener gets passed on to child projects.

--- GenerateMsBuildTask.cs
+++ GenerateMsBuildTask.cs
@@ -9,32 +9,56 @@ using NAnt.Core.Attributes;
 namespace GenerateMsBuildTask
 {
     [TaskName("generate-msbuild")]
-    public class GenerateMsBuildTask : Task
+    public class GenerateMsBuildTask : Task, IBuildListener
     {
         protected override void ExecuteTask()
         {
+            Project.BuildStarted += BuildStarted;
             Project.BuildFinished += BuildFinished;
+            Project.TargetStarted += TargetStarted;
+            Project.TargetFinished += TargetFinished;
+            Project.TaskStarted += TaskStarted;
             Project.TaskFinished += TaskFinished;
+            Project.MessageLogged += MessageLogged;
+
+            // this ensures we are propagated to child projects
+            Project.BuildListeners.Add(this);
         }
 
-        protected void BuildFinished(object sender, BuildEventArgs args)
+        public void BuildFinished(object sender, BuildEventArgs e)
         {
-
         }
 
-        protected void TaskFinished(object sender, BuildEventArgs args)
+        public void BuildStarted(object sender, BuildEventArgs e)
         {
-            if (args.Exception == null)
+        }
+
+        public void MessageLogged(object sender, BuildEventArgs e)
+        {
+        }
+
+        public void TargetFinished(object sender, BuildEventArgs e)
+        {
+        }
+
+        public void TargetStarted(object sender, BuildEventArgs e)
+        {
+        }
+
+        public void TaskFinished(object sender, BuildEventArgs e)
+        {
+            if (e.Exception == null)
             {
-                switch(args.Task.Name)
+                switch (e.Task.Name) // TODO: handle other standard tasks as well
                 {
                     case "csc": break;
                     case "resgen": break;
-                    case "mkdir": break;
-                    case "delete": break;
-                    case "nant": break; // TODO: this has to be handled in TaskStarted to properly handle chaining
                 }
             }
+        }
+
+        public void TaskStarted(object sender, BuildEventArgs e)
+        {
         }
     }
 }
